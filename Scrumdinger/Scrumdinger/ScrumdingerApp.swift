968b990b7934eee8e05a86666e454e982982d093
Marcel Fagadariu <Marcel.Fagadariu@ikano.se>
2023-10-31 10:11:39 +0200

    include project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Marcel on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
