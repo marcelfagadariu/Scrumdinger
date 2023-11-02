1f3908a09d8c269e95cdc976b5a6a62656a12aee
Marcel Fagadariu <Marcel.Fagadariu@ikano.se>
2023-11-02 10:48:31 +0200

    wip

---
+++ Scrumdinger/Scrumdinger/Model/DailyScrum.swift
@@ -0,0 +1,33 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Marcel on 02.11.2023.
+//
+
+import Foundation
+
+struct DailyScrum {
+    let title: String
+    let attendees: [String]
+    let lengthInMinutes: Int
+    let theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design",
+                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                   lengthInMinutes: 10,
+                   theme: .yellow),
+        DailyScrum(title: "App Dev",
+                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                   lengthInMinutes: 5,
+                   theme: .orange),
+        DailyScrum(title: "Web Dev",
+                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                   lengthInMinutes: 5,
+                   theme: .poppy)
+    ]
+}
---
+++ Scrumdinger/Scrumdinger/Model/Theme.swift
@@ -0,0 +1,38 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by Marcel on 02.11.2023.
+//
+
+import SwiftUI
+
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
