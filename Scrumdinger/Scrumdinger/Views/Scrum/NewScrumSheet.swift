ca49bc909ea0f690dd743969b94dbba50ab9e1b5
Marcel Fagadariu <Marcel.Fagadariu@ikano.se>
2023-11-13 17:24:36 +0200

    Finish component

---
+++ Scrumdinger/Scrumdinger/Views/Scrum/NewScrumSheet.swift
@@ -0,0 +1,39 @@
+//
+//  NewScrumSheet.swift
+//  Scrumdinger
+//
+//  Created by Marcel on 13.11.2023.
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+
+    @State private var newScrum = DailyScrum.emptyScrum
+    @Binding var scrums: [DailyScrum]
+    @Binding var isPresentingNewScrumView: Bool
+
+    var body: some View {
+        NavigationStack {
+            DetailEditView(scrum: $newScrum)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Dismiss") {
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Add") {
+                            scrums.append(newScrum)
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    NewScrumSheet(scrums: .constant(DailyScrum.sampleData),
+                  isPresentingNewScrumView: .constant(true))
+}
