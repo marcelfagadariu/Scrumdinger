5676ec1476edc6fa2b968dd551dc8face9c9a20a
Marcel Fagadariu <Marcel.Fagadariu@ikano.se>
2023-11-14 10:02:14 +0200

    Display error view

---
+++ Scrumdinger/Scrumdinger/Model/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by Marcel on 14.11.2023.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
