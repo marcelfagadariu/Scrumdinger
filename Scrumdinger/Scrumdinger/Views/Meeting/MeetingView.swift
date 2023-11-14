2a9bc0546d68477faf6d552863327e15e88d87e1
Marcel Fagadariu <march_sb_16@yahoo.com>
2023-11-14 21:27:20 +0200

    added transcript

--- Scrumdinger/Scrumdinger/Views/Meeting/MeetingView.swift
+++ Scrumdinger/Scrumdinger/Views/Meeting/MeetingView.swift
@@ -12,6 +12,8 @@ struct MeetingView: View {
 
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
     private var player: AVPlayer { AVPlayer.sharedDingPlayer }
 
     var body: some View {
@@ -22,8 +24,7 @@ struct MeetingView: View {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
                                   secondsRemaining: scrumTimer.secondsRemaining,
                                   theme: scrum.theme)
-                Circle()
-                    .strokeBorder(lineWidth: 24)
+                MeetingTimerView(isRecording: isRecording, speakers: scrumTimer.speakers, theme: scrum.theme)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
             .onAppear {
@@ -44,12 +45,18 @@ struct MeetingView: View {
             player.seek(to: .zero)
             player.play()
         }
+        speechRecognizer.resetTranscript()
+        speechRecognizer.startTranscribing()
+        isRecording = true
         scrumTimer.startScrum()
     }
 
     private func endScrum() {
         scrumTimer.stopScrum()
-        let newHistory = History(attendees: scrum.attendees)
+        speechRecognizer.stopTranscribing()
+        isRecording = false
+        let newHistory = History(attendees: scrum.attendees,
+                                 transcript: speechRecognizer.transcript)
         scrum.history.insert(newHistory, at: 0)
     }
 }
